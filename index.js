b6bf586e1d8bf8b39b42126edff8dd9d7acfefd5
ILW8 <daohengliu@gmail.com>
2025-01-24 13:58:41 -0800

    add support infra for collecting heartrate metrics

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import WebSocketManager from './js/socket.js';
+import ReconnectingWebSocket from './js/reconnecting-websocket.min'
 
 
 const cache = {};
-const url = new URL(`${window.location.origin}${window.location.pathname}${window.location.hash.replace('#', '?')}`);
-
+const baseUrl = window.location.origin + window.location.pathname;
+const basePath = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl;
 
 let host = "127.0.0.1:24050" || window.location.host;
 const socket = new WebSocketManager(host);
@@ -38,7 +39,10 @@ function on_open() {
         console.log(error);
     }
 
-    setInterval(updatePromFile, 497);
+    setInterval(updateSubs, 498);
+    setInterval(updateTimeRemaining, 497);
+
+    setInterval(updatePromSubathonMetrics, 999);
 }
 
 async function on_commands(data) {
@@ -58,17 +62,22 @@ async function on_commands(data) {
 
 socket.createConnection(`/websocket/commands`, on_commands, undefined, on_open);
 
-async function updatePromFile() {
+let subs = null;
+let timerSeconds = null;
+
+async function updateSubs() {
+    const timerStr = await (await fetch(`${basePath}/subathon_evolved/clock.txt`)).text();
+    const [hours, minutes, seconds] = timerStr.split(':').map(Number);
+    timerSeconds = hours * 3600 + minutes * 60 + seconds;
+}
+
+async function updateTimeRemaining() {
+    subs = await (await fetch(`${basePath}/subathon_evolved/subscriptions.txt`)).text();
+}
+
+async function updatePromSubathonMetrics() {
     if (!cache["promPushGatewayURL"])
         return;
-
-    const baseUrl = window.location.origin + window.location.pathname;
-    const basePath = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl;
-    const timerStr = await (await fetch(`${basePath}/subathon_evolved/clock.txt`)).text();
-    const subs = await (await fetch(`${basePath}/subathon_evolved/subscriptions.txt`)).text();
-    const [hours, minutes, seconds] = timerStr.split(':').map(Number);
-    const timerSeconds = hours * 3600 + minutes * 60 + seconds;
-    console.log(`timer is ${timerStr} (${timerSeconds}s), subs is ${subs}`);
 
     let output = '';
     output += '# HELP farmathon_timer_remaining_seconds Time left in the farmathon timer\n';
@@ -87,9 +96,31 @@ async function updatePromFile() {
         },
         body: output,
     }).then((resp) => {
-        if (resp.ok)
-            console.log('Metrics pushed OK')
-        else
+        if (!resp.ok)
+            console.error(`failed to push metrics: ${resp.status} ${resp.statusText}`)
+    }).catch((err) => {
+        console.error(`error pushing metrics: ${err}`)
+    })
+}
+
+async function updatePromHeartrateMetric(heartrate) {
+        if (!cache["promPushGatewayHeartrateURL"])
+        return;
+
+    let output = '';
+    output += '# HELP heartrate Latest heartrate reading from pulsoid\n';
+    output += '# TYPE heartrate gauge\n';
+    output += `heartrate ${heartrate}\n`;
+
+    fetch(cache["promPushGatewayHeartrateURL"], {
+        method: 'PUT',
+        // mode: 'no-cors',
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+        body: output,
+    }).then((resp) => {
+        if (!resp.ok)
             console.error(`failed to push metrics: ${resp.status} ${resp.statusText}`)
     }).catch((err) => {
         console.error(`error pushing metrics: ${err}`)
